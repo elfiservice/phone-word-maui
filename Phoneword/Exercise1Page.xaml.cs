ff16a1afce1f95e270d3da03a19f116b0ff278e7
Armando Jr <elfiservice@hotmail.com>
2024-01-15 07:53:01 -0300

    provided event handlers to code behind  ExercisePage1

--- Phoneword/Exercise1Page.xaml.cs
+++ Phoneword/Exercise1Page.xaml.cs
@@ -2,8 +2,29 @@ namespace Phoneword;
 
 public partial class Exercise1Page : ContentPage
 {
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
 	public Exercise1Page()
 	{
 		InitializeComponent();
+
+		if (File.Exists(_fileName))
+        {
+            editorNote.Text = File.ReadAllText(_fileName);
+        }
 	}
+
+	void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, editorNote.Text);
+    }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+        editorNote.Text = string.Empty;
+    }
 }
