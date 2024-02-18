9e2a8dab18f480769aff1888647ea3a3c728818b
Armando Jr <elfiservice@hotmail.com>
2024-02-18 09:52:56 -0300

    provided Standard page as home and provided register the route to Custom Page

--- Phoneword/AppShell.xaml.cs
+++ Phoneword/AppShell.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace Phoneword;
+﻿using Phoneword.Pages.SharedResourcesEx1;
+
+namespace Phoneword;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(CustomTipPage), typeof(CustomTipPage));
+
+        Application.Current.UserAppTheme = AppTheme.Light;
 	}
 }
